d0c4a899cd9ab7a697419daed46148e9681eb2b5
Saadhundekari <110806050+HUndekariSaad@users.noreply.github.com>
2025-07-01 14:43:38 +0530

    Added dark/light theme support with CoreUI dropdown and icon integration in header

--- src/app/website/layout/header/header.component.ts
+++ src/app/website/layout/header/header.component.ts
@@ -1,16 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
 import { Renderer2 } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { AvatarComponent, BadgeComponent, BreadcrumbRouterComponent, ColorModeService, ContainerComponent, DropdownComponent, DropdownDividerDirective, DropdownHeaderDirective, DropdownItemDirective, DropdownMenuDirective, DropdownToggleDirective, HeaderNavComponent, HeaderTogglerDirective, NavItemComponent, NavLinkDirective, SidebarToggleDirective } from '@coreui/angular';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [ CommonModule,RouterModule],
+  imports: [ CommonModule,RouterModule,ContainerComponent, HeaderTogglerDirective, SidebarToggleDirective, IconDirective, HeaderNavComponent, NavItemComponent, NavLinkDirective, RouterLink, RouterLinkActive, NgTemplateOutlet, BreadcrumbRouterComponent, DropdownComponent, DropdownToggleDirective, AvatarComponent, DropdownMenuDirective, DropdownHeaderDirective, DropdownItemDirective, BadgeComponent, DropdownDividerDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-constructor(private renderer: Renderer2) {}
+constructor(private renderer: Renderer2,public colorModeService: ColorModeService) {}
   setDarkMode() {
     this.renderer.removeClass(document.body, 'light-mode');
     this.renderer.addClass(document.body, 'dark-mode');
@@ -20,4 +22,20 @@ constructor(private renderer: Renderer2) {}
     this.renderer.removeClass(document.body, 'dark-mode');
     this.renderer.addClass(document.body, 'light-mode');
   }
+
+
+
+    readonly #colorModeService = inject(ColorModeService);
+  readonly colorMode = this.#colorModeService.colorMode;
+
+  readonly colorModes = [
+    { name: 'light', text: 'Light', icon: 'cilSun' },
+    { name: 'dark', text: 'Dark', icon: 'cilMoon' },
+    { name: 'auto', text: 'Auto', icon: 'cilContrast' }
+  ];
+
+  readonly icons = computed(() => {
+    const currentMode = this.colorMode();
+    return this.colorModes.find(mode => mode.name === currentMode)?.icon ?? 'cilSun';
+  });
 }
