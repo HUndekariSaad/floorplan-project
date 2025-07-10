991e8429b7401da07c493123501c3a4c863d23d3
Vaishnavi Itape <156821431+VaishnaviItape@users.noreply.github.com>
2025-07-10 15:42:19 +0530

    done the Ui of the home master

--- src/app/views/Master/home master/home-drawer/home-drawer.component.ts
+++ src/app/views/Master/home master/home-drawer/home-drawer.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-drawer',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './home-drawer.component.html',
   styleUrl: './home-drawer.component.scss'
 })
 export class HomeDrawerComponent {
+   @Input() drawerVisible: boolean = false;
+  @Input() drawerClose: () => void = () => {};
+  homeData = {
+    description: '',
+    image: null
+  };
+  drawervisible=false;
 
+
+  previewImage: string | ArrayBuffer | null = null;
+
+  save(form: any): void {
+    if (!this.homeData.description.trim()) {
+      alert('Please enter Description.');
+      return;
+    }
+    if (!this.homeData.image) {
+      alert('Please upload an Image.');
+      return;
+    }
+
+    alert('Home Master Saved Successfully!');
+    this.clearForm(form);
+  }
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = e => this.previewImage = reader.result;
+      reader.readAsDataURL(file);
+      this.homeData.image = file;
+    }
+  }
+
+  clearForm(form: any): void {
+    this.homeData = { description: '', image: null };
+    this.previewImage = null;
+    form.resetForm();
+  }
 }
--- src/app/views/Master/home master/home-master/home-master.component.ts
+++ src/app/views/Master/home master/home-master/home-master.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { HomeDrawerComponent } from '../home-drawer/home-drawer.component';
 
 @Component({
   selector: 'app-home-master',
-  imports: [],
+  imports: [HomeDrawerComponent],
   templateUrl: './home-master.component.html',
   styleUrl: './home-master.component.scss'
 })
 export class HomeMasterComponent {
+  formTitle = "Home Master"
+  homeList = [
+    { description: 'Welcome to Our Website', imageUrl: 'https://via.placeholder.com/100' },
+    { description: 'Explore Our Services', imageUrl: '' }
+  ];
+
+  edit(item: any): void {
+    alert('Edit clicked: ' + item.description);
+  }
+
+  delete(item: any): void {
+    const index = this.homeList.indexOf(item);
+    if (index !== -1) {
+      this.homeList.splice(index, 1);
+    }
+  }
+   drawerTitle = "Add New picture";
+  // drawerData: CustmoerCategoryData = new CustmoerCategoryData();
+  drawervisible = false;
+  add(){
+     this.drawerTitle = "Add new Picture";
+    // this.drawerData = new CustmoerCategoryData();
+    this.drawervisible = true;
+  }
+  drawerClose(): void {
+  this.drawervisible = false;
+}
+
+  closeCallback = () => {
+  this.drawerClose();
+};
 
 }
