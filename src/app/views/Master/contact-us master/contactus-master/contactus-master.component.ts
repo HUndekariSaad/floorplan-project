9837ed9357a0c4b56a955b8dc2d8fa0a5c633b83
Saadhundekari <110806050+HUndekariSaad@users.noreply.github.com>
2025-07-14 13:02:44 +0530

    feat(contact-us): integrate form submission and fetch data functionality

--- src/app/views/Master/contact-us master/contactus-master/contactus-master.component.ts
+++ src/app/views/Master/contact-us master/contactus-master/contactus-master.component.ts
@@ -1,11 +1,84 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contactus-master',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contactus-master.component.html',
   styleUrl: './contactus-master.component.scss'
 })
 export class ContactusMasterComponent {
+ formTitle = "Home Master"
+  apiKey: string = 'api/HomeMaster';
+  HomeList: any  = [];
 
+  dummyContactList = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    phone: '9876543210',
+    service: ['Epoxy Flooring', 'Tile Protection'],
+    message: 'I need epoxy flooring for my warehouse.'
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    email: 'jane.smith@example.com',
+    phone: '9123456789',
+    service: ['PU Coating', 'Self-leveling Screed'],
+    message: 'Interested in PU coating and screed service.'
+  },
+  {
+    firstName: 'Rahul',
+    lastName: 'Patel',
+    email: 'rahul.patel@example.com',
+    phone: '9988776655',
+    service: ['Under Water Coating'],
+    message: 'Need coating for water tank.'
+  }
+];
+
+ constructor(private api: ApiService) {
+
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+ 
+
+
+  
+
+  edit(item: any): void {
+    alert('Edit clicked: ' + item.description);
+  }
+
+  delete(item: any): void {
+    const index = this.HomeList.indexOf(item);
+    if (index !== -1) {
+      this.HomeList.splice(index, 1);
+    }
+  }
+   drawerTitle = "Add New picture";
+  // drawerData: CustmoerCategoryData = new CustmoerCategoryData();
+  drawervisible = false;
+  add(){
+     this.drawerTitle = "Add new Picture";
+    this.drawervisible = true;
+  }
+  drawerClose(): void {
+  this.drawervisible = false;
+}
+
+  closeCallback = () => {
+  this.drawerClose();
+};
+  getData() {
+    this.api.getDataApi(this.apiKey).subscribe((res: any) => {
+      this.HomeList = res;
+    })
+  }
 }
--- src/app/website/views/contact-us/contact-us.component.ts
+++ src/app/website/views/contact-us/contact-us.component.ts
@@ -1,29 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import Swal from 'sweetalert2';
 import { ApiService } from '../../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
-imports: [RouterModule,ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule,CommonModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
   contactForm: FormGroup;
+  servicesList: string[] = [
+    'Epoxy Flooring',
+    'PU Coating',
+    'Self-leveling Screed',
+    'Tile Protection',
+    'All Wall Coatings',
+    'Under Water Coating'
+  ];
 
 
-  constructor(private fb: FormBuilder, private http: HttpClient,private api: ApiService,) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private api: ApiService,) {
     this.contactForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phoneNumber: ['', Validators.required],
-      service: ['', Validators.required],
+      service: this.fb.array([], Validators.required),
       message: ['', Validators.required]
     });
+
+
   }
 
   onSubmit() {
@@ -44,5 +55,18 @@ export class ContactUsComponent {
     });
   }
 
+onCheckboxChange(event: any) {
+  const serviceArray: FormArray = this.contactForm.get('service') as FormArray;
 
+  if (event.target.checked) {
+    serviceArray.push(this.fb.control(event.target.value));
+  } else {
+    const index = serviceArray.controls.findIndex(x => x.value === event.target.value);
+    if (index >= 0) serviceArray.removeAt(index);
+  }
+}
+
+  getServiceId(service: string): string {
+    return service.replace(/ /g, '-');
+  }
 }
